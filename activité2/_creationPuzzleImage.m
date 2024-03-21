6b666939a964973db03269577fbc413bf970b079
Lois Genay <159648649+lolosk@users.noreply.github.com>
2024-03-21 13:54:36 +0100

    Add files via upload

---
+++ activité2/_creationPuzzleImage.m
@@ -0,0 +1,28 @@
+function imPuzzle = _creationPuzzleImage(imageIn)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Fonction creationPuzzleImage
+%
+% Fonction cachée permettant de créer l'image Puzzle à partir de l'image
+% d'origine
+%
+% ******************** Syntaxe ********************
+% imageOut = creationPuzzleImage(imageIn);
+%
+%	imageIn : image d'origine (tableau de données de taille LxCx1 ou LxCx3)
+%	imageOut : image Puzzle (tableau de données de taille ?x?x1 ou ?x?x3)
+% *************************************************
+%
+%	Remarque 1 : Attention à la taille de l'image Puzzle, ce n'est pas la
+% même que celle de l'image d'origine.
+%
+%	Remarque 2 : Cette fonction utilise des fonctions aléatoires. Relancer
+% le code changera l'image Puzzle.
+%
+%	Remarque 3 : La fonction renvoie une image du même type que l'image
+%	d'origine (double, uint8, etc)
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+end
